c89509c2af11980fe4e53584fdca2b4fe603406d
user <user@user-pc>
2016-11-11 14:13:34 +0900

    두번째 커밋

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!!");
-		System.out.println("¾È³çÇÏ¼¼¿ä~~");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”~~");
 	}
 
 }
