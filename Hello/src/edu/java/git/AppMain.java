5beaf68d25d47fdf3298518c7ecedf7037007859
user <user@user-pc>
2016-11-11 15:14:33 +0900

    버그 픽스

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!!");
-		System.out.println("안녕하세요~~");
+		System.out.println("버그 픽스");
 		System.out.println("한글이 깨진다");
 	}
 
